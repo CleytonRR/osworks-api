fde3949c8cc83778d92f90bcd5dc7ec3f85a7646
cleytonRR <cleytonrodrigues69@live.com>
2020-06-18 15:46:54 -0300

    Install spring-data-jpa, mysql-connector, flyway configure database and migrations. create controller

--- src/main/java/com/algaworks/osworks/api/controller/ClientController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClientController.java
@@ -1,27 +1,24 @@
 package com.algaworks.osworks.api.controller;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ClientController {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Jose");
-        cliente1.setEmail("jose@gmail.com");
-        cliente1.setTelefone("99910101010");
-        Cliente cliente2 = new Cliente();
-        cliente1.setId(2L);
-        cliente1.setNome("Maria");
-        cliente1.setEmail("Maria@gmail.com");
-        cliente1.setTelefone("88810101010");
-        return Arrays.asList(cliente1, cliente2);
+        return clienteRepository.findAll();
     }
 }
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,7 +1,15 @@
 package com.algaworks.osworks.domain.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
     private String telefone;
@@ -36,5 +44,18 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
