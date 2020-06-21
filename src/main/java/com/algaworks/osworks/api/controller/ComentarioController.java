30bdcb931c56ec4cb846313cbfe9474c2ffb9b95
cleytonRR <cleytonrodrigues69@live.com>
2020-06-21 14:50:01 -0300

    Created endpoint to list comments

--- src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ComentarioController.java
@@ -3,6 +3,9 @@ package com.algaworks.osworks.api.controller;
 import com.algaworks.osworks.api.model.Comentario;
 import com.algaworks.osworks.api.model.ComentarioInput;
 import com.algaworks.osworks.api.model.ComentarioModel;
+import com.algaworks.osworks.domain.exceptions.EntityNotFoundException;
+import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.CrudOrderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +13,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/ordens-servico/{comentarioServicoId}/comentarios")
+@RequestMapping("/ordens-servico/{ordemServicoId}/comentarios")
 public class ComentarioController {
 
     @Autowired
@@ -21,15 +26,31 @@ public class ComentarioController {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
+    @GetMapping
+    public List<ComentarioModel> listar(@PathVariable Long ordemServicoId) {
+        OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+                .orElseThrow(() -> new EntityNotFoundException("Ordem de serviço não encotrada"));
+
+        return toModelCollection(ordemServico.getComentarios());
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ComentarioModel adicionar(@PathVariable Long comentarioServicoId,
+    public ComentarioModel adicionar(@PathVariable Long ordemServicoId,
                                      @Valid @RequestBody ComentarioInput comentarioInput) {
-        Comentario comentario = crudOrderService.adicionarComentario(comentarioServicoId, comentarioInput.getDescricao());
+        Comentario comentario = crudOrderService.adicionarComentario(ordemServicoId, comentarioInput.getDescricao());
         return this.toModel(comentario);
     }
 
     private ComentarioModel toModel(Comentario comentario) {
         return modelMapper.map(comentario, ComentarioModel.class);
+    }
+
+    private List<ComentarioModel> toModelCollection(List<Comentario> comentarios) {
+        return comentarios.stream().map(comentario -> this.toModel(comentario))
+                .collect(Collectors.toList());
     }
 }
